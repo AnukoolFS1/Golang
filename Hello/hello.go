188e46b55aa40b4c15c21a3bcd5d128fc5710353
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-03-19 20:38:39 +0530

    Brief overview of Error handling in Go

--- Hello/hello.go
+++ Hello/hello.go
@@ -3,14 +3,41 @@ package main
 import (
 	"example.com/greetings"
 	"fmt"
+	"log"
 )
 
-func main() {
-	// Get a greeting message and print it.
-	text := greetings.Hello("Gladys")
-	fmt.Println(text)
-	message := WheelWonder("Anukool")
 
-	fmt.Printf("This is the %v", message)
-	fmt.Println(greetings.AnyNumer)
+
+func main() {
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // Request a greeting message.
+    message, err := greetings.Hello("")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
+    fmt.Println(message)
 }
+
+// func main() {
+// 	// Get a greeting message and print it.
+// 	text := greetings.Hello("Gladys")
+// 	fmt.Println(text)
+// 	message := WheelWonder("Anukool")
+
+// 	fmt.Printf("This is the %v", message)
+// 	fmt.Println(greetings.AnyNumer)
+// }
+
+/// Printf for dynamic string of changes
+/// Sprintf for Printf but with returned value
+/// Println just for console check
--- greetings/greetins.go
+++ greetings/greetins.go
@@ -1,10 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 var AnyNumer = 2
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hello, %v. Welcome!", name)
-	return message
+	return message, nil
 }
