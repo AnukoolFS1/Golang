c220d6206c1ebb2429fb4d1d78bf022f524f6cb3
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-03-22 14:56:30 +0530

    coding done

--- Hello/hello.go
+++ Hello/hello.go
@@ -14,19 +14,40 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
-	message, err := greetings.Hello("Anukool")
-	// If an error was returned, print it to the console and
-	// exit the program.
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// If no error was returned, print the returned message
-	// to the console.
-	fmt.Println(message)
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
 
+//----------------------------------------------------------------------------------
+// func main() {
+// 	// Set properties of the predefined Logger, including
+// 	// the log entry prefix and a flag to disable printing
+// 	// the time, source file, and line number.
+// 	log.SetPrefix("greetings: ")
+// 	log.SetFlags(0)
+
+// 	// Request a greeting message.
+// 	message, err := greetings.Hello("Anukool")
+// 	// If an error was returned, print it to the console and
+// 	// exit the program.
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	// If no error was returned, print the returned message
+// 	// to the console.
+// 	fmt.Println(message)
+// }
+//----------------------------------------------------------------------------------
 // func main() {
 // 	// Get a greeting message and print it.
 // 	text := greetings.Hello("Gladys")
