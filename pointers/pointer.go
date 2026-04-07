983b86a48b0d23908e2329f66e082b34eef0d8a4
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-04-07 15:55:16 +0530

    Starting pointer

--- pointers/pointer.go
+++ pointers/pointer.go
@@ -3,18 +3,22 @@ package main
 import "fmt"
 
 func PointerUnderstanding() {
-	i, j := 42, 2701
+	i := 42
+	p := &i
 
-	p := &i         // point to i
-	fmt.Println(*p) // read i through the pointer
-	*p = 21         // set i through the pointer
-	fmt.Println(i)  // see the new value of i
-
-	p = &j         // point to j
-	*p = *p / 37   // divide j through the pointer
-	fmt.Println(j) // see the new value of j
+	fmt.Println(p)
 }
 
 func main() {
+	// i, j := 42, 2701
+
+	// p := &i         // point to i
+	// fmt.Println(*p) // read i through the pointer
+	// *p = 21         // set i through the pointer
+	// fmt.Println(i)  // see the new value of i
+
+	// p = &j         // point to j
+	// *p = *p / 37   // divide j through the pointer
+	// fmt.Println(j) // see the new value of j
 	PointerUnderstanding()
 }
