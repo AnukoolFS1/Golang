efb98f479a9196be8d3b240b85747857611de919
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-04-07 12:47:29 +0530

    defer done

--- defer/defer.go
+++ defer/defer.go
@@ -1,1 +1,16 @@
-package defer
+package main
+
+import "fmt"
+
+func practiceDefer() {
+	deferingThing := func() {
+		defer fmt.Println("done")
+		fmt.Println("working...")
+	}
+	defer deferingThing()
+}
+
+func main() {
+	practiceDefer()
+	fmt.Println("initiating...")
+}
---
+++ defer/go.mod
@@ -0,0 +1,3 @@
+module anukool/defer
+
+go 1.26.1
