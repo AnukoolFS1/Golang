2bf959c24654a43edbbb6ac0ca095f2f88d14ec3
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-04-14 19:07:31 +0530

    forgotten the structs way of working

--- practice/structs/structs.go
+++ practice/structs/structs.go
@@ -1,1 +1,16 @@
-package mmain
+package main
+
+
+// import "fmt"
+
+// func main() {
+
+// 	struct Person = {
+// 		name string
+// 		age int8
+// 	}
+
+// 	var person1 = Person{name: "Anukool", age:}
+// }
+
+// need to comeback here
