315ab2811a9a89d062624c3858b82d88bee5927a
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-04-17 21:29:13 +0530

    tried to understand the dx dy of image but failed

--- practice/ArrayS/slices.go
+++ practice/ArrayS/slices.go
@@ -14,15 +14,29 @@ func testingSlice() {
 	fmt.Printf("slicePointer %p\n", slicePointer)
 }
 
+// func main() {
+
+// 	// var arr [10]int = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+// 	// var slicedArr1 = arr[4:]
+// 	// var slicedArr2 = arr[:4]
+
+// 	// fmt.Printf("%v\n", slicedArr1)
+// 	// fmt.Printf("%v\n", slicedArr2)
+// 	// testingSlice()
+// 	SlicesOfSlice()
+// 	fmt.Println(nil)
+
+// 	data:= []int{0,1,2,3,4}
+// 	// range
+
+// }
 func main() {
-
-	// var arr [10]int = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
-
-	// var slicedArr1 = arr[4:]
-	// var slicedArr2 = arr[:4]
-
-	// fmt.Printf("%v\n", slicedArr1)
-	// fmt.Printf("%v\n", slicedArr2)
-	// testingSlice()
-	SlicesOfSlice()
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i) // == 2**i
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
 }
