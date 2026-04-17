8da06ccc4b4a653bc636851a619f0e310da35ceb
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-04-17 15:41:15 +0530

    going back to slices for its practice

--- practice/structs/structs.go
+++ practice/structs/structs.go
@@ -12,18 +12,31 @@ func main() {
 
 	var value = Vertex{"Black", 2, false}
 
-	var value2 = Vertex{"anukool", 3, true}
+	var value2 = Vertex{X:"anukool", Y: 3}
 
 	fmt.Printf("%+v\n", value)
 	fmt.Printf("%v\n", value2)
 
-	fmt.Println(value.X)
-	fmt.Println(value.Y)
-	fmt.Println(value2.X)
-	fmt.Println(value2.Y)
+	// fmt.Println(value.X)
+	// fmt.Println(value.Y)
+	// fmt.Println(value2.X)
+	// fmt.Println(value2.Y)
 
-	fmt.Println(value.Z)
-	fmt.Println(value2.Z)
+	// fmt.Println(value.Z)
+	// fmt.Println(value2.Z)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(s)
 
 }
 
