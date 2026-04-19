c3aa0cf03dc268894813ada8bcbb8e7ed5974f1f
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-04-19 22:02:04 +0530

    maps

--- practice/structs/structs.go
+++ practice/structs/structs.go
@@ -10,34 +10,28 @@ func main() {
 		Z bool
 	}
 
-	var value = Vertex{"Black", 2, false}
+	// var value = Vertex{"Black", 2, false}
 
-	var value2 = Vertex{X:"anukool", Y: 3}
+	// var value2 = Vertex{X:"anukool", Y: 3}
 
-	fmt.Printf("%+v\n", value)
-	fmt.Printf("%v\n", value2)
+	// // fmt.Printf("%+v\n", value)
+	// // fmt.Printf("%v\n", value2)
 
-	// fmt.Println(value.X)
-	// fmt.Println(value.Y)
-	// fmt.Println(value2.X)
-	// fmt.Println(value2.Y)
 
-	// fmt.Println(value.Z)
-	// fmt.Println(value2.Z)
-
-	s := []struct {
-		i int
-		b bool
-	}{
-		{2, true},
-		{3, false},
-		{5, true},
-		{7, true},
-		{11, false},
-		{13, true},
+	/// MAPS ///
+	type VertexTwo struct {
+		Lat, Long float64
 	}
-	fmt.Println(s)
 
+	var m map[string]VertexTwo
+
+	m = make(map[string]VertexTwo)
+
+	m["Bell Labs"] = VertexTwo{
+		40.68433, -74.39967,
+	}
+
+	fmt.Println(m)
 }
 
 // need to comeback here
