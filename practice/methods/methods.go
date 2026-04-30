73f08ea5317c210cd0ab137c13687358093f166b
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-04-30 21:12:31 +0530

    some more methods without struct

--- practice/methods/methods.go
+++ practice/methods/methods.go
@@ -5,16 +5,30 @@ import (
 	"math"
 )
 
-type Vertex struct {
-	X, Y float64
-}
+// type Vertex struct {
+// 	X, Y float64
+// }
 
-func (v Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+// func (v Vertex) Abs() float64 {
+// 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+// }
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
 }
 
 func main() {
-	v := Vertex{3,4}
+	// v := Vertex{3,4}
 
-	fmt.Println(v.Abs())
+	// fmt.Println(v.Abs())
+
+
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+
 }
