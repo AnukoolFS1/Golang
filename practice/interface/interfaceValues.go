3bdc91378cbb928617b4500cc8a35b07946636ee
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-05-19 20:19:32 +0530

    interface and underlying values

--- practice/interface/interfaceValues.go
+++ practice/interface/interfaceValues.go
@@ -1,9 +1,48 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// type I interface {
+// 	M()
+// }
+
+// type Ti struct {
+// 	S string
+// }
+
+// func (t *Ti) M() {
+// 	fmt.Println(t.S)
+// }
+
+// type Fi float64
+
+// func (f Fi) M() {
+// 	fmt.Println(f)
+// }
+
+// func main() {
+// 	var i I
+
+// 	i = &Ti{"Hello"}
+// 	describe(i)
+// 	i.M()
+
+// 	i = Fi(math.Pi)
+// 	describe(i)
+// 	i.M()
+// }
+
+// func describe(i I) {
+// 	fmt.Printf("(%v, %T)\n", i, i)
+// }
+
+
 package main
 
-import (
-	"fmt"
-	// "math"
-)
+import "fmt"
 
 type I interface {
 	M()
@@ -14,18 +53,25 @@ type T struct {
 }
 
 func (t *T) M() {
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
 	fmt.Println(t.S)
 }
 
-type F float64
+func main() {
+	var i I
 
-func (f F) M() {
-	fmt.Println(f)
+	describe(i)
+	var t *T
+	i = t
+	i.M()
+
+	i = &T{"hello"}
+	describe(i)
+	i.M()
 }
-
-// func main() {
-
-// }
 
 func describe(i I) {
 	fmt.Printf("(%v, %T)\n", i, i)
