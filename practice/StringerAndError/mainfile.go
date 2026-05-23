99c0a26577a5e63f4ece54ccb4413c873dbafeca
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-05-23 12:27:43 +0530

    type declaration check inside another function block

--- practice/StringerAndError/mainfile.go
+++ practice/StringerAndError/mainfile.go
@@ -7,24 +7,31 @@ type Person struct {
 	age int
 }
 
-type SimplePerson Person
+// type SimplePerson Person
 
 func(person *Person) String() string {
 	return fmt.Sprintf("%s, (%d years old)", person.name, person.age)
+}
+
+func(person Person) PrintSimple() {
+	type SimplePerson Person
+	fmt.Println(SimplePerson(person))
 }
 
 func main() {
 	person1 := &Person{"Anukool", 27}
 	person2 := Person{"AnukoolSecond", 28}
 
-	simplePerson := SimplePerson(*person1)
+	// simplePerson := SimplePerson(*person1)
 
 	fmt.Println(person1) // result -> Anukool, (27 years old)
 	fmt.Println(person2) // result -> {Anukool2 28}
-	fmt.Printf("%+v\n",simplePerson)
+	// fmt.Printf("%+v\n",simplePerson)
 
 
-	if err:= run(1); err != nil{
-		fmt.Println(err)
-	}
+	// if err:= run(1); err != nil{
+	// 	fmt.Println(err)
+	// }
+
+	person1.PrintSimple()
 }
