6400e3f98fd65434b15353b90cee8f8ae562a7d3
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-06-02 17:51:00 +0530

    starting the images

--- practice/images/images.go
+++ practice/images/images.go
@@ -1,40 +1,13 @@
 package main
 
-import (
-	"image"
-	"image/color"
-	"image/png"
-	"os"
-)
+// --------the interface of image
 
-func LoadImage() {
-	file, err := os.Open("photo.png")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	// img, err := png.Decode(file)
-	// if err != nil {
-	// 	panic(err)
-	// }
-}
+// type Image interface{
+// 	ColorModel() color.Model
+//     Bounds() Rectangle
+//     At(x, y int) color.Color
+// }
 
 func main() {
-	img := image.NewRGBA(
-		image.Rect(0, 0, 100, 100),
-	)
 
-	red := color.RGBA{255, 0, 0, 255}
-
-	for y := 0; y < 100; y++ {
-		for x := 0; x < 100; x++ {
-			img.Set(x, y, red)
-		}
-	}
-
-	file, _ := os.Create("red.png")
-	defer file.Close()
-
-	png.Encode(file, img)
 }
