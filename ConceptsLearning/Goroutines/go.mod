eef53f519db1d2a15cb865a6e00e011c624fc55f
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-06-13 10:21:14 +0530

    range and goroutine

---
+++ ConceptsLearning/Goroutines/go.mod
@@ -0,0 +1,3 @@
+module anukool.practice/goroutines
+
+go 1.26.1
--- ConceptsLearning/Goroutines/goroutines.go
+++ ConceptsLearning/Goroutines/goroutines.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ { // range 5 or range ragne 1 << 1
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	go say("world")
+	say("hello")
+}
