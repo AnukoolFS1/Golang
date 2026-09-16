05e8ec6c6139ebbaee2b0f12bdf8053644ff4d77
Anukool Singh Chauhan <ascmailing0@gmail.com>
2026-09-16 07:57:45 +0530

    Database pgx.Exec and pgx.Query

--- Database/main.go
+++ Database/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"context"
+	"fmt"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -16,8 +16,16 @@ func main() {
 		panic(err)
 	}
 
+	_, err = conn.Exec(ctx, `DROP TABLE newtable;`)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Println(string(value))
+
 	defer conn.Close(ctx)
 
-	fmt.Println(conn)
 	fmt.Println("Postgres is connected")
+	fmt.Println("Table is created")
 }
